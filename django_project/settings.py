250a79112b28b394f8fb5bce42673d8398007ba9
jocnn <ijann_@outlook.com>
2022-07-08 12:18:11 -0300

    se configuró los archivos para subirlo a heroku

--- django_project/settings.py
+++ django_project/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = str(os.getenv("SECRET_KEY"))
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "whitenoise.runserver_nostatic",
-    "blog.apps.BlogConfig", 
+    "blog.apps.BlogConfig",
     "accounts.apps.AccountsConfig",
 ]
 
